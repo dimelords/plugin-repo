155fe453f586a07df2c5d3db5ec09ed64f50e501
Fredrik Gustafsson <fredrik@teamleader.se>
2024-12-30 08:14:51 +0100

    customplugin

---
+++ manifests/custom.manifest.js
@@ -0,0 +1,20 @@
+export default {
+    name: 'custom',
+    version: '1.0.0',
+    displayName: 'Custom Dynamic Plugin',
+    description: 'A plugin that uses Chart.js',
+    author: 'Fredrik Gustafsson',
+    entryPoint: 'customPlugin.jsx',
+    dependencies: [],
+    packages: [
+      {
+        name: 'chart.js',
+        version: '4.4.1',
+        globalName: 'Chart',
+        url: 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js'
+      }
+    ],
+    config: {
+      theme: 'purple'
+    }
+  };
---
+++ plugins/customPlugin.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef } from "react";
+import { BasePlugin } from "./basePlugin";
+
+class CustomPluginBase extends BasePlugin {
+  renderContent(props) {
+    return (
+      <CustomPluginContent
+        {...props}
+        manifest={this.manifest}
+        config={this.getConfig()}
+      />
+    );
+  }
+
+  render() {
+    return this.renderContent();
+  }
+}
+
+function CustomPluginContent({ manifest, config }) {
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current && window.Chart) {
+      const ctx = chartRef.current.getContext("2d");
+
+      chartInstance.current = new window.Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow"],
+          datasets: [
+            {
+              label: "My Chart",
+              data: [12, 19, 3],
+            },
+          ],
+        },
+      });
+    }
+
+    return () => {
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+        chartInstance.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <div className={`p-4 bg-${config.theme}-100 rounded`}>
+      <h2 className="font-bold">{manifest.displayName}</h2>
+      <canvas ref={chartRef}></canvas>
+    </div>
+  );
+}
+
+export default CustomPluginBase;
